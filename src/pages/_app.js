b1746ac979d809ef1b4ec8a905852d17403dd543
marcelo1811 <miyachimarcelo@gmail.com>
2021-03-05 23:57:50 -0300

    fix: remove React Fragment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'React'
 import GlobalStyle from '../styles/GlobalStyle'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <React.Fragment>
+    <>
       <GlobalStyle />
       <Component {...pageProps} />
-    </React.Fragment>
+    </>
   )
 
 }
